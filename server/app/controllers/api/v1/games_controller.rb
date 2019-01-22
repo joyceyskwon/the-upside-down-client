9ce571ee5748592f76033c7e0b7eef5e78fd540d
github username <github email address>
2019-01-22 02:15:03 -0500

    merged frontend changes from joyce branch

--- server/app/controllers/api/v1/games_controller.rb
+++ server/app/controllers/api/v1/games_controller.rb
@@ -5,4 +5,15 @@ class Api::V1::GamesController < ApplicationController
     render json: GameSerializer.new(@game), status: :ok
   end
 
+  def create
+    @game = Game.create(game_params)
+    render json: @game, status: :created
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:user_id, :win)
+  end
+
 end
